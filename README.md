874ce5f6f3e0e4733e53a08b90bcf60bc6354e69
Alex Michael Berry <alexmiberry@gmail.com>
2023-12-15 00:00:21 -0500

    0.2.0

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 A vite plugin pretending to be a static site generator.
 
 Note: This repo contains the original POC code and isn't usuable yet.
+
+## Project Structure
+
+Nail Clipper Screwdriver expects the following project layout
+
+| File or Directory | Comment                              |
+| ----------------- | ------------------------------------ |
+| ./src/layouts/    | Holds HTML5 + Mustache templates.    | 
+| ./index.md        | The home page.                       |
+| ./public/         | Optional folder of static resources. |
+| ./vite.config.js  | Vite's config.                       |
+
+## Layouts
+
+Layouts should be complete HTML 5 templates with mustache tags for the title and body.
+Usually you'll want to wrap title in two sets of braces and the body in three.
+
+## Content
+
+Content in Nail Clipper Screwdriver is stored in spicy *.md files.
+Besides all the markdown features the top of the file contains a frontmatter section.
+This section  
--- index.js
+++ index.js
@@ -1,60 +1,45 @@
-import { resolve, basename } from 'path';
+import { basename, resolve } from 'path';
 import fs from 'fs';
 import { marked } from 'marked';
 import Mustache from 'mustache';
-import { globSync } from 'glob'
+import { globSync } from 'glob';
 
-export default function nailClipperScrewdriver() {
+/**
+ * Takes a single config object with the following fields
+ *  - layout: the default layout
+ *  - data: globalData available to all pages
+ */
+export function nailClipperScrewdriver(config) {
   return {
     name: 'nailer-clipper-screwdriver',
 
     buildStart(inputOptions) {
-      let htmlFiles = processMustache();
-      htmlFiles = htmlFiles.concat(processDocs());
-      inputOptions.input = htmlFiles;
+      inputOptions.input = processDocs(config);
     },
 
     // Handles live code changes to content files.
     handleHotUpdate(ctx) {
       console.log(`In HHU ${ctx}`);
     },
-  }
+  };
 }
 
-function processMustache() {
-  let htmlFiles = []
-
-  let layoutTemplate = fs.readFileSync(resolve(__dirname, './layout/Main.mustache'), 'utf-8');
-  let indexFile = resolve(__dirname, './index.mustache');
-  let indexTemplate = fs.readFileSync(indexFile, 'utf-8');
-
-  let blergs = globSync('blerg/**.md').map(file => {
-    let url = '/blerg/' + basename(file).replace('.md', '.html');
-    let content = fs.readFileSync(file, 'utf-8');
-    let data = processContent(content);
-    return { date: data.date, url:url, title: data.title };
-  });
-
-  let data = processContent(indexTemplate);
-  data.posts = blergs;
-
-  data['body'] = Mustache.render(data.body, data);
-  let output = Mustache.render(layoutTemplate, data);
-
-  indexFile = indexFile.replace('.mustache', '.html');
-  fs.writeFileSync(indexFile, output);
-  htmlFiles.push(indexFile);
-  return htmlFiles;
-}
-
-function processDocs() {
+function processDocs(config) {
   let htmlFiles = [];
-  let paths = globSync('./blerg/**.md');
+  let paths = globSync('**/*.md', { ignore: ['node_modules/**', 'README.md'] });
 
   paths.forEach(function (path) {
     let data = processContent(fs.readFileSync(path, 'utf-8'));
-    let template = fs.readFileSync(resolve(__dirname, 'layout/Blerg.mustache'), 'utf-8');
-    let output = Mustache.render(template, data);
+    data.data = config.data;
+    let layout = data.layout ? data.layout : config.layout;
+    let layoutTemplate = fs.readFileSync(
+      resolve(__dirname, './src/layout/' + layout),
+      'utf-8',
+    );
+
+    data['body'] = Mustache.render(data.body, data);
+    let output = Mustache.render(layoutTemplate, data);
+
     let fileName = path.replace('.md', '.html');
     htmlFiles.push(fileName);
     fs.writeFileSync(fileName, output);
@@ -62,12 +47,12 @@ function processDocs() {
   return htmlFiles;
 }
 
-function processContent(content) {
-  let data = {}
+export function processContent(content) {
+  let data = {};
   let lines = content.split(/\r?\n/);
   if (lines[0] === '---') { // This file contains metadata
-    content = ''
-    let endHeader = false
+    content = '';
+    let endHeader = false;
     for (const [i, line] of lines.entries()) {
       if (endHeader) {
         content += line + '\n';
